e8638c3cca686c32735d3ce4a7c5a0d2ce9a696f
candelasanzm <sanzmartincandela@gmail.com>
2024-02-22 18:06:12 +0100

    Metodología S003

---
+++ es/uah/matcomp/ej1/Circle.java
@@ -0,0 +1,32 @@
+package es.uah.matcomp.ej1;
+
+public class Circle {
+    private double radius = 1.0;
+
+    public Circle(){
+    }
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return radius*radius*Math.PI;
+    }
+
+    public double getCircumference(){
+        return 2*Math.PI*radius;
+    }
+
+    public String toString(){
+        return "es.uah.matcomp.ej1.Circle[radius = " + radius + "]";
+    }
+}
---
+++ es/uah/matcomp/ej1/ClaseAccount.java
@@ -0,0 +1,57 @@
+package es.uah.matcomp.ej1;
+
+public class ClaseAccount {
+    private String id ;
+    private String name;
+    private int balance = 0;
+
+    public ClaseAccount (String id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public ClaseAccount (String id, String name, int balance){
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public int credit (int amount){
+        balance = balance + amount;
+        return balance;
+    }
+
+    public int debit (int amount){
+        if (amount <= balance){
+
+        }
+        else
+            System.out.println("Amount exceeded balance");
+        return balance;
+    }
+
+    public int transferTo (ClaseAccount another, int amount){
+        if (amount <= balance){
+
+        }
+        else
+            System.out.println("Amount exceeded balance");
+        return balance;
+    }
+
+    public String toString(){
+        return "Account[id = " + id + " name = " + name + "balance = " + balance + "]";
+    }
+}
