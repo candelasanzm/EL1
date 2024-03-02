55a2f4d24f113a3330dca2a4483abb7033d73cf4
candelasanzm <sanzmartincandela@gmail.com>
2024-03-02 18:28:45 +0100

    Metodología EL1

--- SOO5/Interfaces/MainShapeCircleRectangleSquare.java
+++ SOO5/Interfaces/MainShapeCircleRectangleSquare.java
@@ -13,7 +13,7 @@ public class MainShapeCircleRectangleSquare {
         System.out.println(s1.getPerimeter()); // which version?
         System.out.println(s1.getColor());
         System.out.println(s1.isFilled());
-        System.out.println(s1.getRadius());
+        System.out.println(((Circle) s1).getRadius());
 
         Circle c1 = (Circle)s1; // Downcast back to Circle
         System.out.println(c1);
@@ -30,7 +30,7 @@ public class MainShapeCircleRectangleSquare {
         System.out.println(s3.getArea());
         System.out.println(s3.getPerimeter());
         System.out.println(s3.getColor());
-        System.out.println(s3.getLength());
+        System.out.println(((Rectangle) s3).getLength());
 
         Rectangle r1 = (Rectangle)s3; // downcast
         System.out.println(r1);
@@ -42,7 +42,7 @@ public class MainShapeCircleRectangleSquare {
         System.out.println(s4);
         System.out.println(s4.getArea());
         System.out.println(s4.getColor());
-        System.out.println(s4.getSide());
+        System.out.println(((Square) s4).getSide());
 
 // Take note that we downcast Shape s4 to Rectangle,
 // which is a superclass of Square, instead of Square
